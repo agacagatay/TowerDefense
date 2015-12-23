1a6b5febeab0468c95cd85daf4d1332b140de832
Kevin Salchert <kevinsalchert@Kevin-Mac-Mini.local>
2015-12-23 11:55:23 -0800

    Turret will now aim at specified target

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour
+{
+	[SerializeField] Transform targetTransform;
+	[SerializeField] Transform horPivot;
+	[SerializeField] Transform verPivot;
+	[SerializeField] float horLookSpeed;
+	[SerializeField] float verLookSpeed;
+
+	void Update()
+	{
+		Quaternion newHorRotation = Quaternion.LookRotation(targetTransform.position - horPivot.position);
+		newHorRotation.x = 0f;
+		newHorRotation.z = 0f;
+
+		Quaternion newVerRotation = Quaternion.LookRotation(targetTransform.position - verPivot.position);
+		newVerRotation.y = 0f;
+		newVerRotation.z = 0f;
+
+		horPivot.localRotation = Quaternion.Lerp(horPivot.localRotation, newHorRotation, Time.deltaTime * horLookSpeed);
+		verPivot.localRotation = Quaternion.Lerp(verPivot.localRotation, newVerRotation, Time.deltaTime * verLookSpeed);
+	}
+}

4597a2f4266be8f414f0ec2f58fd550730371ac2
Kevin Salchert <kevin.salchert@me.com>
2015-12-24 15:55:37 -0800

    Turret spawns at designed positions with tap

---
+++ Assets/Scripts/TurretSpawnController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretSpawnController : MonoBehaviour
+{
+	[SerializeField] GameObject turretPrefab;
+	[SerializeField] Transform spawnTransform;
+
+	void OnEnable()
+	{
+		EasyTouch.On_SimpleTap += On_SimpleTap;
+	}
+
+	void OnDisable()
+	{
+		UnsubscribeEvent();
+	}
+
+	void OnDestroy()
+	{
+		UnsubscribeEvent();
+	}
+
+	void UnsubscribeEvent()
+	{
+		EasyTouch.On_SimpleTap -= On_SimpleTap;	
+	}
+
+	private void On_SimpleTap(Gesture gesture)
+	{
+		if (gesture.pickedObject == gameObject)
+		{
+			Instantiate(turretPrefab, spawnTransform.position, spawnTransform.rotation);
+			gameObject.SetActive(false);
+		}
+	}
+}

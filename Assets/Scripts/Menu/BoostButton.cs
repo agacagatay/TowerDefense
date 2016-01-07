132d58c761df26fc0178c9e733339e1f66987a8b
Kevin Salchert <kevinsalchert@Kevin-Mac-Mini.local>
2016-01-07 11:12:58 -0800

    Boosts can now be triggered

---
+++ Assets/Scripts/Menu/BoostButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostButton : MonoBehaviour
+{
+	[SerializeField] UITexture boostIcon;
+	[SerializeField] UITexture boostDarkIcon;
+
+	void OnClick()
+	{
+		if (BoostController.instance.CanActivate)
+			StartCoroutine(ActivateBoost(BoostController.instance.BoostCooldown));
+	}
+
+	IEnumerator ActivateBoost(float cooldownTime)
+	{
+		BoostController.instance.ActivateBoost();
+		BoostController.instance.CanActivate = false;
+		boostIcon.enabled = false;
+		boostDarkIcon.fillAmount = 0f;
+
+		while (boostDarkIcon.fillAmount < 1f)
+		{
+			boostDarkIcon.fillAmount += Time.deltaTime / cooldownTime;
+
+			if (boostDarkIcon.fillAmount >= 1f)
+			{
+				boostDarkIcon.fillAmount = 1f;
+				boostIcon.enabled = true;
+			}
+
+			yield return null;
+		}
+
+		BoostController.instance.CanActivate = true;
+	}
+}
---
+++ Assets/Scripts/Other/BoostController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostController : MonoBehaviour
+{
+	bool canActivate = true;
+	float boostCooldown = 4f;
+	public bool CanActivate { get { return canActivate; } set { canActivate = value; }}
+	public  float BoostCooldown { get { return boostCooldown; }}
+
+	public static BoostController instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void ActivateBoost()
+	{
+		if (canActivate)
+			Debug.Log("Boost Activated");
+	}
+}

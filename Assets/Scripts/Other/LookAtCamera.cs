233d190f9b0ee2f20c0d19f8bc424a0697580100
Kevin Salchert <kevin.salchert@me.com>
2016-01-29 00:59:03 -0800

    Optimized enemy icon graphic

---
+++ Assets/Scripts/Other/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+	Transform lookAtTransorm;
+
+	void Start()
+	{
+		lookAtTransorm = Camera.main.transform;
+	}
+
+	void Update()
+	{
+		transform.LookAt(lookAtTransorm);
+	}
+}

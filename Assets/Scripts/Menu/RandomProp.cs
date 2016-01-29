89ed18c1825035cf646be06a5d41af9607fee6f8
Kevin Salchert <kevin.salchert@me.com>
2016-01-28 21:28:04 -0800

    Main menu prop selection is now random, graphics optimizations

---
+++ Assets/Scripts/Menu/RandomProp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomProp : MonoBehaviour
+{
+	[SerializeField] GameObject[] propObjets;
+
+	void Awake()
+	{
+		int randomProp = Random.Range(0, propObjets.Length);
+		propObjets[randomProp].SetActive(true);
+	}
+}

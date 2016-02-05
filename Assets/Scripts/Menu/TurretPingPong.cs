47e1a60e34a8584c487e87d63cc6f218aa3a4452
Kevin Salchert <kevinsalchert@Kevin-Mac-Mini.local>
2016-02-04 17:04:16 -0800

    Created tower upgrade menu

---
+++ Assets/Scripts/Menu/TurretPingPong.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretPingPong : MonoBehaviour
+{
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float startRotationAngle;
+	[SerializeField] float minRotationAngle;
+	[SerializeField] float maxRotationAngle;
+	float currentRotationAngle;
+	bool increaseAngle = true;
+
+	void Start()
+	{
+		transform.localRotation = Quaternion.Euler(0f, startRotationAngle, 0f);
+		currentRotationAngle = startRotationAngle;
+	}
+
+	void Update()
+	{
+		if (increaseAngle)
+		{
+			currentRotationAngle += Time.deltaTime * rotationSpeed;
+
+			if (currentRotationAngle >= maxRotationAngle)
+				increaseAngle = false;
+		}
+		else
+		{
+			currentRotationAngle -= Time.deltaTime * rotationSpeed;
+
+			if (currentRotationAngle <= minRotationAngle)
+				increaseAngle = true;
+		}
+
+		transform.localRotation = Quaternion.Euler(0f, currentRotationAngle, 0f);
+	}
+}

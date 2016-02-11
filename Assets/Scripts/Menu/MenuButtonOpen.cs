40d9b5e68e1b08da97845ba62408b2976eb34aed
Kevin Salchert <kevinsalchert@Kevin-Mac-Mini.local>
2016-02-11 14:04:47 -0800

    Integrated menu system for social sharing, added vibration toggle button in Options menu

---
+++ Assets/Scripts/Menu/MenuButtonOpen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonOpen : MonoBehaviour
+{
+	[SerializeField] GameObject objectToDisable;
+
+	void OnClick()
+	{
+		objectToDisable.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Menu/MenuVibrationToggle.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuVibrationToggle : MonoBehaviour
+{
+	[SerializeField] UILabel buttonText;
+	[SerializeField] GameObject enabledCheckbox;
+	[SerializeField] GameObject disabledCheckbox;
+
+	void Start()
+	{
+		int vibrationMode = EncryptedPlayerPrefs.GetInt("VibrationMode", 1);
+
+		if (vibrationMode == 1)
+		{
+			SetStateEnabled(false);
+		}
+		else
+		{
+			SetStateDisabled();
+		}
+	}
+
+	public void ToggleVibration()
+	{
+		int vibrationMode = EncryptedPlayerPrefs.GetInt("VibrationMode", 1);
+
+		if (vibrationMode == 1)
+		{
+			SetStateDisabled();
+		}
+		else
+		{
+			SetStateEnabled(true);
+		}
+	}
+
+	void SetStateEnabled(bool vibrate)
+	{
+		EncryptedPlayerPrefs.SetInt("VibrationMode", 1);
+		buttonText.text = "Enabled";
+		enabledCheckbox.SetActive(true);
+		disabledCheckbox.SetActive(false);
+		PlayerPrefs.Save();
+
+		if (vibrate)
+			Handheld.Vibrate();
+	}
+
+	void SetStateDisabled()
+	{
+		EncryptedPlayerPrefs.SetInt("VibrationMode", 0);
+		buttonText.text = "Disabled";
+		enabledCheckbox.SetActive(false);
+		disabledCheckbox.SetActive(true);
+		PlayerPrefs.Save();
+	}
+}
